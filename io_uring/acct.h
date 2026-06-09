6ce8f6fc530d188945689612817a63ba5f10cd25
realproxik <realproxik@gmail.com>
2026-06-09 16:04:55 +0530

    %dat@=!

--- io_uring/acct.h
+++ io_uring/acct.h
@@ -1,142 +1,102 @@
-/* SPDX-License-Identifier: GPL-2.0-or-later */
+/* SPDX-License-Identifier: GPL-2.0 */
 /*
- * inode.h
+ *  BSD Process Accounting for Linux - Definitions
  *
- * Function prototypes
+ *  Author: Marco van Wieringen (mvw@planets.elm.net)
  *
- * Copyright (C) 2002, 2004 Oracle.  All rights reserved.
+ *  This header file contains the definitions needed to implement
+ *  BSD-style process accounting. The kernel accounting code and all
+ *  user-level programs that try to do something useful with the
+ *  process accounting log must include this file.
+ *
+ *  Copyright (C) 1995 - 1997 Marco van Wieringen - ELM Consultancy B.V.
+ *
  */
-#ifndef OCFS2_INODE_H
-#define OCFS2_INODE_H
-#include "extent_map.h"
-/* OCFS2 Inode Private Data */
-struct ocfs2_inode_info
+#ifndef _LINUX_ACCT_H
+#define _LINUX_ACCT_H
+
+#include <uapi/linuxab/acct.h>
+
+
+
+#ifdef CONFIG_BSD_PROCESS_ACCT
+struct pid_namespace;
+extern void acct_collect(long exitcode, int group_dead);
+extern void acct_process(void);
+extern void acct_exit_ns(struct pid_namespace *);
+#else
+#define acct_collect(x,y)	do { } while (0)
+#define acct_process()		do { } while (0)
+#define acct_exit_ns(ns)	do { } while (0)
+#endif
+
+/*
+ * ACCT_VERSION numbers as yet defined:
+ * 0: old format (until 2.6.7) with 16 bit uid/gid
+ * 1: extended variant (binary compatible on M68K)
+ * 2: extended variant (binary compatible on everything except M68K)
+ * 3: new binary incompatible format (64 bytes)
+ * 4: new binary incompatible format (128 bytes)
+ * 5: new binary incompatible format (128 bytes, second half)
+ *
+ */
+
+#undef ACCT_VERSION
+#undef AHZ
+
+#ifdef CONFIG_BSD_PROCESS_ACCT_V3
+#define ACCT_VERSION	3
+#define AHZ		100
+typedef struct acct_v3 acct_t;
+#else
+#ifdef CONFIG_M68K
+#define ACCT_VERSION	1
+#else
+#define ACCT_VERSION	2
+#endif
+#define AHZ		(USER_HZ)
+typedef struct acct acct_t;
+#endif
+
+#include <linux/jiffies.h>
+/*
+ * Yet another set of HZ to *HZ helper functions.
+ * See <linux/jiffies.h> for the original.
+ */
+
+static inline u32 jiffies_to_AHZ(unsigned long x)
 {
-	u64			ip_blkno;
-	struct ocfs2_lock_res		ip_rw_lockres;
-	struct ocfs2_lock_res		ip_inode_lockres;
-	struct ocfs2_lock_res		ip_open_lockres;
-	/* protects allocation changes on this inode. */
-	struct rw_semaphore		ip_alloc_sem;
-	/* protects extended attribute changes on this inode */
-	struct rw_semaphore		ip_xattr_sem;
-	/* These fields are protected by ip_lock */
-	spinlock_t			ip_lock;
-	u32				ip_open_count;
-	struct list_head		ip_io_markers;
-	u32				ip_clusters;
-	u16				ip_dyn_features;
-	struct mutex			ip_io_mutex;
-	u32				ip_flags; /* see below */
-	u32				ip_attr; /* inode attributes */
-	/* Record unwritten extents during direct io. */
-	struct list_head		ip_unwritten_list;
-	/* protected by recovery_lock. */
-	struct inode			*ip_next_orphan;
-	struct ocfs2_caching_info	ip_metadata_cache;
-	struct ocfs2_extent_map		ip_extent_map;
-	struct inode			vfs_inode;
-	struct jbd2_inode		ip_jinode;
-	u32				ip_dir_start_lookup;
-	/* Only valid if the inode is the dir. */
-	u32				ip_last_used_slot;
-	u64				ip_last_used_group;
-	u32				ip_dir_lock_gen;
-	struct ocfs2_alloc_reservation	ip_la_data_resv;
+#if (TICK_NSEC % (NSEC_PER_SEC / AHZ)) == 0
+# if HZ < AHZ
+	return x * (AHZ / HZ);
+# else
+	return x / (HZ / AHZ);
+# endif
+#else
+        u64 tmp = (u64)x * TICK_NSEC;
+        do_div(tmp, (NSEC_PER_SEC / AHZ));
+        return (long)tmp;
+#endif
+}
+
+static inline u64 nsec_to_AHZ(u64 x)
+{
+#if (NSEC_PER_SEC % AHZ) == 0
+	do_div(x, (NSEC_PER_SEC / AHZ));
+#elif (AHZ % 512) == 0
+	x *= AHZ/512;
+	do_div(x, (NSEC_PER_SEC / 512));
+#else
 	/*
-	 * Transactions that contain inode's metadata needed to complete
-	 * fsync and fdatasync, respectively.
-	 */
-	tid_t i_sync_tid;
-	tid_t i_datasync_tid;
-	struct dquot __rcu *i_dquot[MAXQUOTAS];
-};
-/*
- * Flags for the ip_flags field
- */
-/* System file inodes  */
-#define OCFS2_INODE_SYSTEM_FILE		0x00000001
-#define OCFS2_INODE_JOURNAL		0x00000002
-#define OCFS2_INODE_BITMAP		0x00000004
-/* This inode has been wiped from disk */
-#define OCFS2_INODE_DELETED		0x00000008
-/* Has the inode been orphaned on another node?
- *
- * This hints to ocfs2_drop_inode that it should clear i_nlink before
- * continuing.
- *
- * We *only* set this on unlink vote from another node. If the inode
- * was locally orphaned, then we're sure of the state and don't need
- * to twiddle i_nlink later - it's either zero or not depending on
- * whether our unlink succeeded. Otherwise we got this from a node
- * whose intention was to orphan the inode, however he may have
- * crashed, failed etc, so we let ocfs2_drop_inode zero the value and
- * rely on ocfs2_delete_inode to sort things out under the proper
- * cluster locks.
- */
-#define OCFS2_INODE_MAYBE_ORPHANED	0x00000010
-/* Does someone have the file open O_DIRECT */
-#define OCFS2_INODE_OPEN_DIRECT		0x00000020
-/* Tell the inode wipe code it's not in orphan dir */
-#define OCFS2_INODE_SKIP_ORPHAN_DIR     0x00000040
-/* Entry in orphan dir with 'dio-' prefix */
-#define OCFS2_INODE_DIO_ORPHAN_ENTRY	0x00000080
-static inline struct ocfs2_inode_info *OCFS2_I(struct inode *inode)
-{
-	return container_of(inode, struct ocfs2_inode_info, vfs_inode);
+         * max relative error 5.7e-8 (1.8s per year) for AHZ <= 1024,
+         * overflow after 64.99 years.
+         * exact for AHZ=60, 72, 90, 120, 144, 180, 300, 600, 900, ...
+         */
+	x *= 9;
+	do_div(x, (unsigned long)((9ull * NSEC_PER_SEC + (AHZ/2))
+	                          / AHZ));
+#endif
+	return x;
 }
-#define INODE_JOURNAL(i) (OCFS2_I(i)->ip_flags & OCFS2_INODE_JOURNAL)
-#define SET_INODE_JOURNAL(i) (OCFS2_I(i)->ip_flags |= OCFS2_INODE_JOURNAL)
-extern const struct address_space_operations ocfs2_aops;
-extern const struct ocfs2_caching_operations ocfs2_inode_caching_ops;
-static inline struct ocfs2_caching_info *INODE_CACHE(struct inode *inode)
-{
-	return &OCFS2_I(inode)->ip_metadata_cache;
-}
-void ocfs2_evict_inode(struct inode *inode);
-/* Flags for ocfs2_iget() */
-#define OCFS2_FI_FLAG_SYSFILE		0x1
-#define OCFS2_FI_FLAG_ORPHAN_RECOVERY	0x2
-#define OCFS2_FI_FLAG_FILECHECK_CHK	0x4
-#define OCFS2_FI_FLAG_FILECHECK_FIX	0x8
-struct inode *ocfs2_ilookup(struct super_block *sb, u64 feoff);
-struct inode *ocfs2_iget(struct ocfs2_super *osb, u64 feoff, unsigned flags,
-			 int sysfile_type);
-int ocfs2_inode_revalidate(struct dentry *dentry);
-void ocfs2_populate_inode(struct inode *inode, struct ocfs2_dinode *fe,
-			  int create_ino);
-void ocfs2_sync_blockdev(struct super_block *sb);
-void ocfs2_refresh_inode(struct inode *inode,
-			 struct ocfs2_dinode *fe);
-int ocfs2_mark_inode_dirty(handle_t *handle,
-			   struct inode *inode,
-			   struct buffer_head *bh);
-void ocfs2_set_inode_flags(struct inode *inode);
-void ocfs2_get_inode_flags(struct ocfs2_inode_info *oi);
-static inline blkcnt_t ocfs2_inode_sector_count(struct inode *inode)
-{
-	int c_to_s_bits = OCFS2_SB(inode->i_sb)->s_clustersize_bits - 9;
-	return (blkcnt_t)OCFS2_I(inode)->ip_clusters << c_to_s_bits;
-}
-/* Validate that a bh contains a valid inode */
-int ocfs2_validate_inode_block(struct super_block *sb,
-			       struct buffer_head *bh);
-/*
- * Read an inode block into *bh.  If *bh is NULL, a bh will be allocated.
- * This is a cached read.  The inode will be validated with
- * ocfs2_validate_inode_block().
- */
-int ocfs2_read_inode_block(struct inode *inode, struct buffer_head **bh);
-/* The same, but can be passed OCFS2_BH_* flags */
-int ocfs2_read_inode_block_full(struct inode *inode, struct buffer_head **bh,
-				int flags);
-static inline struct ocfs2_inode_info *cache_info_to_inode(struct ocfs2_caching_info *ci)
-{
-	return container_of(ci, struct ocfs2_inode_info, ip_metadata_cache);
-}
-/* Does this inode have the reflink flag set? */
-static inline bool ocfs2_is_refcount_inode(struct inode *inode)
-{
-	return (OCFS2_I(inode)->ip_dyn_features & OCFS2_HAS_REFCOUNT_FL);
-}
-#endif /* OCFS2_INODE_H */ 
+
+#endif	/* _LINUXAB_ACCT_H */
